d0fadf10cec3249e61616e6b4e52128e26547b58
Woodman502 <joewoodcox@icloud.com>
2015-09-24 18:54:47 -0400

    Added Spock and LIzard class without method logic

---
+++ Homeworks/Homework4/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/Lizard.java
@@ -0,0 +1,20 @@
+package com.wesleyreisz.rockpaperscissors.Game;
+
+import com.wesleyreisz.rockpaperscissors.GameUtils;
+import com.wesleyreisz.rockpaperscissors.R;
+
+/**
+ * Created by Joe Woodcox on 9/24/2015.
+ */
+public class Lizard implements GameType{
+    @Override
+    public String eval(Integer opponentChoice) {
+        if (opponentChoice== R.id.btnPaper){
+            return GameUtils.BEATS;
+        }else if (opponentChoice==R.id.btnRock){
+            return GameUtils.LOSES_TO;
+        }
+        else{
+            return GameUtils.TIES;
+        }
+}
