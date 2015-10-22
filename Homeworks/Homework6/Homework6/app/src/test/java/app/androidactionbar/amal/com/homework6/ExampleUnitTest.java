522de81c9c07ff320bebf30fc38db6643906bc1d
Woodman502 <joewoodcox@icloud.com>
2015-10-22 13:40:09 -0400

    Added new homework6 project

---
+++ Homeworks/Homework6/Homework6/app/src/test/java/app/androidactionbar/amal/com/homework6/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package app.androidactionbar.amal.com.homework6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
