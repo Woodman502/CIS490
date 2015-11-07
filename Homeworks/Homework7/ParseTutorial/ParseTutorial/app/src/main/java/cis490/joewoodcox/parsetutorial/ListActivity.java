fdcaca99e9d07adc32703718e31c997b12eaae1d
Woodman502 <joewoodcox@icloud.com>
2015-11-07 12:28:01 -0500

    Finished Part1 of Parse tutorial

--- Homeworks/Homework7/ParseTutorial/ParseTutorial/app/src/main/java/cis490/joewoodcox/parsetutorial/ListActivity.java
+++ Homeworks/Homework7/ParseTutorial/ParseTutorial/app/src/main/java/cis490/joewoodcox/parsetutorial/ListActivity.java
@@ -1,23 +1,23 @@
 package cis490.joewoodcox.parsetutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
 
-public class MainActivity extends AppCompatActivity {
-
+public class ListActivity extends AppCompatActivity {
+    private Button btnCreate;
+    private Button btnUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,30 +32,27 @@ public class MainActivity extends AppCompatActivity {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "tTBmXNXyIfqdQzvW9SLpw2i5fBKuc60eYqxVzQcV", "0yk9YnCBjyi5jf96bsrrmMISgpK5Z4WDCQWZIjTz");
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                Intent createActivity = new Intent(getApplicationContext(), CreateActivity.class);
+                startActivity(createActivity);
+
+            }
+        });
+
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                Intent updateActivity = new Intent(getApplicationContext(), UpdateActivity.class);
+                startActivity(updateActivity);
+
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
