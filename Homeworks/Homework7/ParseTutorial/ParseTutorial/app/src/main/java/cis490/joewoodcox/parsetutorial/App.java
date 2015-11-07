1595f0379e30dfdba8ea576f6bde3e4dcf741d44
Woodman502 <joewoodcox@icloud.com>
2015-11-07 11:47:06 -0500

    Added Clean upload of Parse tutorial start

---
+++ Homeworks/Homework7/ParseTutorial/ParseTutorial/app/src/main/java/cis490/joewoodcox/parsetutorial/App.java
@@ -0,0 +1,7 @@
+package cis490.joewoodcox.parsetutorial;
+
+/**
+ * Created by Joe Woodcox on 11/7/2015.
+ */
+public class App {
+}
---
+++ Homeworks/Homework7/ParseTutorial/ParseTutorial/app/src/test/java/cis490/joewoodcox/parsetutorial/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package cis490.joewoodcox.parsetutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
